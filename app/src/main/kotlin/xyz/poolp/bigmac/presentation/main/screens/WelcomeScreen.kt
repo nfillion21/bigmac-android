2a9a37dc19538abfb934b7e0acedeafa7ef077e8
Nicolas Fillion <nfillion21@gmail.com>
2024-01-04 15:43:06 +0100

    switch name to road

--- app/src/main/kotlin/xyz/poolp/bigmac/presentation/main/screens/WelcomeScreen.kt
+++ app/src/main/kotlin/xyz/poolp/bigmac/presentation/main/screens/WelcomeScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.luminance
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -77,13 +78,15 @@ private fun Logo(
     lightTheme: Boolean = LocalContentColor.current.luminance() < 0.5f,
 ) {
     val assetId = if (lightTheme) {
-        R.drawable.mcdonalds_logo
+        R.drawable.ic_launcher_playstore
     } else {
-        R.drawable.mcdonalds_logo
+        R.drawable.ic_launcher_playstore
     }
     Image(
         painter = painterResource(id = assetId),
-        modifier = modifier.size(128.dp),
+        modifier = modifier
+            .size(128.dp)
+            .clip(MaterialTheme.shapes.extraLarge),
         contentDescription = null
     )
 }
--- buildSrc/src/main/kotlin/ConfigData.kt
+++ buildSrc/src/main/kotlin/ConfigData.kt
@@ -3,6 +3,6 @@ object ConfigData {
     const val compileSdkVersion = 34
     const val minSdkVersion = 31
     const val targetSdkVersion = 34
-    const val versionCode = 1
-    const val versionName = "1.0"
+    const val versionCode = 2
+    const val versionName = "1.1"
 }
